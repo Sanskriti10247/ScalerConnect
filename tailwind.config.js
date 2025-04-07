8d5767786caf9873bc87f5e667e0ebc0fb653ab3
Sanskriti <Sanskriti.24bcs10247@sst.scaler.com>
2025-04-07 15:00:46 +0530

    git commit -m "Updated favicon and minor config changes"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}", // Make sure this points to your code
   ],
   theme: {
     extend: {},
